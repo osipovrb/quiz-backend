0d89b06c761908dab293f4f5ffd7511cc16233a5
panda <panda@panda.panda>
2019-09-03 15:24:18 +0800

    Regenerate token after log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,6 @@ class UsersController < ApplicationController
   def login # post /users/login
     user = User.find_by_username params[:username]
     if user && user.authenticate(params[:password])
-      user.regenerate_token if user.token.blank?
       render json: { token: user.token, username: user.username }
     else
       head 401
@@ -34,7 +33,7 @@ class UsersController < ApplicationController
   end
 
   def logout # delete /users/logout
-    @user.update_attribute(:token, '')
+    @user.update_attribute(:token, User.generate_unique_secure_token)
     head 204
   end
 
