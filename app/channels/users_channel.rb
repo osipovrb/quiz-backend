b23db88c890ce97d389aceed06864dcc6a24b718
panda <panda@panda.panda>
2019-09-19 01:55:39 +0800

    Streaming for current_user added

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -1,5 +1,6 @@
 class UsersChannel < ApplicationCable::Channel
   def subscribed
+  	stream_for current_user
     members = ChatMember.includes(:user).pluck(:username)
     broadcast_to current_user, members.to_json
     stream_from "users"
