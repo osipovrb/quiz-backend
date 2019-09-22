a176014127ec29296106e6e0c1148a386f2bd558
panda <panda@panda.panda>
2019-09-22 18:06:36 +0800

    broadcast_all_members changed format

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -9,7 +9,7 @@ class UsersChannel < ApplicationCable::Channel
 
   private
     def broadcast_all_members
-      members = ChatMember.includes(:user).pluck(:username, :score)
+      members = ChatMember.includes(:user).pluck(:username, :score).map { |u| {username: u[0], score: u[1]} }
       stream_for current_user
       broadcast_to current_user, members.to_json
     end
