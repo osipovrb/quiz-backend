b9b0cf1b01e922d3755ba79735048d359e2d999c
panda <panda@panda.panda>
2019-09-24 00:16:59 +0800

    AllusersBroadcastJob added

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -7,16 +7,17 @@ class UsersChannel < ApplicationCable::Channel
 
   def start
     #unless current_user.instance_variable_defined?(:@users_channel_started)
-      broadcast_all_members
+      #broadcast_all_members
+      AllusersBroadcastJob.perform_later(current_user)
       stream_from 'users'
       #current_user.instance_variable_set(:@users_channel_started, true)
     #end
   end
 
   private
-    def broadcast_all_members
-      members = ChatMember.includes(:user).pluck(:username, :score).map { |u| {username: u[0], score: u[1]} }
-      stream_for current_user
-      broadcast_to current_user, { event: 'current_members', current_members: members }
-    end
+    #def broadcast_all_members
+    #  members = ChatMember.includes(:user).pluck(:username, :score).map { |u| {username: u[0], score: u[1]} }
+    #  stream_for current_user
+    #  broadcast_to current_user, { event: 'current_members', current_members: members }
+    #end
 end
