f1b0041603c467d1d34227dd5c7aa01c89789692
panda <panda@panda.panda>
2019-09-23 21:44:57 +0800

    Typo fixed

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -17,6 +17,6 @@ class UsersChannel < ApplicationCable::Channel
     def broadcast_all_members
       members = ChatMember.includes(:user).pluck(:username, :score).map { |u| {username: u[0], score: u[1]} }
       stream_for current_user
-      broadcast_to current_user, { event: 'current_members', current_memebers: members }.to_json
+      broadcast_to current_user, { event: 'current_members', current_members: members }.to_json
     end
 end
