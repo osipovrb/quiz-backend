1a16c9ae83f90b7073e6c24b1044c6c3e02df904
panda <panda@panda.panda>
2019-09-23 20:41:35 +0800

    #start fix

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -9,7 +9,7 @@ class UsersChannel < ApplicationCable::Channel
     unless current_user.instance_variable_defined?(:@users_channel_started)
       broadcast_all_members
       stream_from 'users'
-      current_user.instance_variable_set(:@chat_channel_started, true)
+      current_user.instance_variable_set(:@users_channel_started, true)
     end
   end
 
@@ -17,6 +17,6 @@ class UsersChannel < ApplicationCable::Channel
     def broadcast_all_members
       members = ChatMember.includes(:user).pluck(:username, :score).map { |u| {username: u[0], score: u[1]} }
       stream_for current_user
-      broadcast_to current_user, members.to_json
+      broadcast_to current_user, { current_memebers: members }.to_json
     end
 end
