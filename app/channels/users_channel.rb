fc450b353ef57eaff8d751516d5d1888bf5843b9
panda <panda@panda.panda>
2019-09-29 19:20:58 +0800

    Fix

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -8,7 +8,9 @@ class UsersChannel < ApplicationCable::Channel
   end
 
   def get_all_members
-    members = ActionCable::Channel.connected_users_for(ChatChannel).pluck(:username, :score).map { |u| { username: u.first, score: u.last } }
+    members = ApplicationCable::Channel.connected_users_for(ChatChannel).
+      pluck(:username, :score).
+      map { |u| { username: u.first, score: u.last } } 
     broadcast_to current_user, { event: 'current_members', current_members: members }
   end
 end
